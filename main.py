96dce5c31ab6c66c1cf9c924f1bedb82de9dbab5
SculptedNYX <DarkValvet@protonmail.com>
2022-12-10 14:37:11 +0200

    Added from,to and destination chooser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import customtkinter, tkinter
+from tkinter.filedialog import askdirectory
 from pytube import YouTube,Playlist
 from PIL import Image
 import requests
@@ -8,8 +9,7 @@ import re
 
 # App meta data
 COLORTHEME = "dark-blue"
-RESOLUTION = "640x480"
-FONT = ("Comic Sans MS",400, "bold")
+RESOLUTION = "640x580"
 
 # App customization
 customtkinter.set_appearance_mode("Dark")
@@ -26,16 +26,14 @@ title_label.pack(anchor=tkinter.CENTER)
 
 frame = customtkinter.CTkFrame(master=app)
 frame.pack(pady=20, padx= 60, fill="both", expand=True)
-
+frame.pack_propagate(False)
 
 # Gets input for video or playlist
 url_label = customtkinter.CTkLabel(frame, text="Youtube URL:")
 url_label.grid(row=0,column=0, pady = (10,0),padx=20)
 
-url_entry = customtkinter.CTkEntry(frame,placeholder_text="www.youtube.com/yourvideo",width=300)
+url_entry = customtkinter.CTkEntry(frame,placeholder_text="www.youtube.com/yourvideo",width=300,fg_color="gray21")
 url_entry.grid(row=0,column=1,columnspan=2,pady = (10,0),padx=10)
-
-global content # Stores the video or playlist
 
 # Content display logic
 
@@ -46,7 +44,7 @@ def display_content(content):
     arb = re.compile(r'[ا-ي]')
     
     #TODO: fix Asian chars crashing
-    #TODO: add unicode characters support
+    #TODO: add emoji support
     
     if eng.match(content.title):
         title = content.title
@@ -79,27 +77,33 @@ def check_button():
     url = url_entry.get()
 
     # Checks if it's shortened
-    if url.count("https://youtu.be/") == 1:
+    if url.count("youtu.be/") == 1:
         url = unshorten_url(url)
 
     # Checks for the type of link
-    if url.count("https://youtube.com/playlist?list=") == 1:
+    if url.count("youtube.com/playlist?list=") == 1 or url.count("&list=") == 1:
         # Updates the label and graps playlist
         url_check_label.configure(text="Playlist detected!")
         content = Playlist(url)
         display_content(content)
+        construct_download(True,str(len(content)))
 
-    elif url.count("https://www.youtube.com/watch?v=") == 1:
+    elif url.count("youtube.com/watch?v=") == 1:
         # Updates the label and graps video
         url_check_label.configure(text="Video detected!")
         content = YouTube(url)
         display_content(content)
+        construct_download(False,"1")
 
     else:
         url_check_label.configure(text="Cannot recognize url type :(")
         content = None
         name_preview_label.configure(text="")
         thumbnail_preview_label.configure(image=None)
+        construct_download(False,"0")
+        
+    select_destination_entry.delete(0,'end')
+
 
 # UI for the check button
 
@@ -121,18 +125,88 @@ thumbnail_preview_label.grid(row=3, column=1)
 
 # Download area
 
-#TODO: ADD FRAME FOR DOWNLOAD
+downloadframe = customtkinter.CTkFrame(master=frame,width=440)
+downloadframe.grid(row=4, columnspan= 2,pady=20, padx= 40)
+downloadframe.grid_propagate(False)
 
+# From to section
+from_range_label = customtkinter.CTkLabel(master=downloadframe,text="From: ")
+from_range_entry = customtkinter.CTkEntry(master=downloadframe,width=125)
+
+from_range_label.grid(row=0,column=0,pady=10, padx=20)
+from_range_entry.grid(row=0,column=1,pady=10)
+from_range_entry.delete(0,'end')
+from_range_entry.configure(fg_color="gray")
+from_range_entry.configure(state=tkinter.DISABLED)
+
+to_range_label = customtkinter.CTkLabel(master=downloadframe,text="To: ")
+to_range_entry = customtkinter.CTkEntry(master=downloadframe,width=125)
+
+to_range_label.grid(row=0,column=2,pady=10, padx=20)
+to_range_entry.grid(row=0,column=3,padx=(0,10),pady=10)
+to_range_entry.delete(0,'end')
+to_range_entry.configure(fg_color="gray")
+to_range_entry.configure(state=tkinter.DISABLED)
+
+
+# Selecting the destination
+def select_folder():
+    path = askdirectory(title="Select your folder")
+    select_destination_entry.delete(0,'end')
+    select_destination_entry.insert(0, path)
+
+select_destination_label = customtkinter.CTkLabel(master=downloadframe,text="destination")
+select_destination_entry = customtkinter.CTkEntry(master=downloadframe,width=125,fg_color="gray21")
+select_destination_button = customtkinter.CTkButton(master=downloadframe,text="select",width=125,command=select_folder)
+
+select_destination_label.grid(row=1,column=0,padx=20,pady=10)
+select_destination_entry.grid(row=1,column=1,pady=10)
+select_destination_button.grid(row=1,column=3,pady=10,padx=(0,10))
+
+# Download button and logic
+video_button = customtkinter.CTkButton(master=downloadframe,text="Download video",width=160)
+video_button.grid(row=2,column=0,columnspan=2,pady=40,padx=20)
+
+audio_button = customtkinter.CTkButton(master=downloadframe,text="Download Audio",width=160)
+audio_button.grid(row=2,column=2,columnspan=2,pady=40,padx=20)
+
+def construct_download(is_playlist,length):
+
+    if(is_playlist):
+        from_range_entry.configure(state=tkinter.NORMAL)
+        from_range_entry.configure(fg_color="gray21")
+        to_range_entry.configure(state=tkinter.NORMAL)
+        to_range_entry.configure(fg_color="gray21")
+
+        from_range_entry.delete(0,'end')
+        from_range_entry.insert(0, "1")
+        to_range_entry.delete(0,'end')
+        to_range_entry.insert(0, length)
+    else:
+        from_range_entry.delete(0,'end')
+        from_range_entry.configure(fg_color="gray")
+        from_range_entry.configure(state=tkinter.DISABLED)
+
+        to_range_entry.delete(0,'end')
+        to_range_entry.configure(fg_color="gray")
+        to_range_entry.configure(state=tkinter.DISABLED)
+        return;
+
+#TODO: Implement logic beind video and audio downloads, add a progressbar and a cancel button and a quality selector
+
+def download_asvideo(content):
+    pass
+
+def download_asaudio(content):
+    pass
 
 
 # Display mode list
 def change_appearance_mode_event(new_appearance_mode):
    customtkinter.set_appearance_mode(new_appearance_mode)
 
-appearance_mode_label = customtkinter.CTkLabel(app, text="Appearance Mode:", anchor="s")
-appearance_mode_label.pack(padx=20, anchor="w")
-appearance_mode_optionemenu = customtkinter.CTkOptionMenu(app, values=["Dark", "Light", "System"], command=change_appearance_mode_event)
-appearance_mode_optionemenu.pack(padx=20, pady=20, anchor="w")
+appearance_mode_optionemenu = customtkinter.CTkOptionMenu(frame, values=["Dark", "Light", "System"], command=change_appearance_mode_event)
+appearance_mode_optionemenu.grid(row=5,padx=20)
 
 
 app.mainloop()
